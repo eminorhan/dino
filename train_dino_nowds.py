fde6344b32690993815cd58f25ffa9c7769b8ff8
eminorhan <aeminorhan@gmail.com>
2023-02-21 11:24:12 -0500

    bug fix: remove all dist dependence in nowds

--- train_dino_nowds.py
+++ train_dino_nowds.py
@@ -326,9 +326,7 @@ class DINOLoss(nn.Module):
         """
         Update center used for teacher output.
         """
-        batch_center = torch.sum(teacher_output, dim=0, keepdim=True)
-        dist.all_reduce(batch_center)
-        batch_center = batch_center / (len(teacher_output) * dist.get_world_size())
+        batch_center = torch.mean(teacher_output, dim=0, keepdim=True)
 
         # ema update
         self.center = self.center * self.center_momentum + batch_center * (1 - self.center_momentum)
