3f2028e26a24459a8e0ca626f44a39b2774bafa0
eminorhan <aeminorhan@gmail.com>
2023-05-24 11:38:28 -0400

    add tsnes

--- scripts/visualize_resnext.sh
+++ scripts/visualize_resnext.sh
@@ -8,22 +8,22 @@
 #SBATCH --output=visualize_resnext_%A_%a.out
 #SBATCH --array=0-4
 
-# srun python -u /scratch/eo41/dino/visualize_resnext.py \
-# 	--data_path '/scratch/eo41/dino/labeled_s_examples/table' \
-# 	--class_idx 23 \
-# 	--n_out 26 \
-# 	--batch_size 8 \
-# 	--seed $SLURM_ARRAY_TASK_ID \
-# 	--pretrained_backbone '/scratch/eo41/dino/models_resnext50/s_5fps_resnext50_checkpoint.pth' \
-# 	--pretrained_fc '/scratch/eo41/dino/evals/labeled_s/s_resnext50_checkpoint.pth.tar'
-
 srun python -u /scratch/eo41/dino/visualize_resnext.py \
-	--data_path '/scratch/eo41/dino/ecoset_examples/woman' \
-	--class_idx 3 \
-	--n_out 565 \
+	--data_path '/scratch/eo41/dino/labeled_s_examples/basket' \
+	--class_idx 1 \
+	--n_out 26 \
 	--batch_size 8 \
 	--seed $SLURM_ARRAY_TASK_ID \
-	--pretrained_backbone '/scratch/eo41/dino/models_resnext50/say_5fps_resnext50_checkpoint.pth' \
-	--pretrained_fc '/scratch/eo41/dino/evals/ecoset/say_resnext50_checkpoint.pth.tar'
+	--pretrained_backbone '/scratch/eo41/dino/models_resnext50/y_5fps_resnext50_checkpoint.pth' \
+	--pretrained_fc '/scratch/eo41/dino/evals/labeled_s/y_resnext50_checkpoint.pth.tar'
+
+# srun python -u /scratch/eo41/dino/visualize_resnext.py \
+# 	--data_path '/scratch/eo41/dino/ecoset_examples/woman' \
+# 	--class_idx 3 \
+# 	--n_out 565 \
+# 	--batch_size 8 \
+# 	--seed $SLURM_ARRAY_TASK_ID \
+# 	--pretrained_backbone '/scratch/eo41/dino/models_resnext50/say_5fps_resnext50_checkpoint.pth' \
+# 	--pretrained_fc '/scratch/eo41/dino/evals/ecoset/say_resnext50_checkpoint.pth.tar'
 
 echo "Done"
