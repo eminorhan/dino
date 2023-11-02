bug fix in vimlp_collosal: remove unnecessary normalization

vision_mlps.py
@@ -24,13 +24,12 @@ class VisionMLP(nn.Module):
         self.flatten = nn.Flatten()
         self.first_layer = nn.Linear(input_size, hidden_features)
         self.act_layer = act_layer
-        self.norm_layer = norm_layer
         self.blocks = nn.ModuleList([Block(hidden_features, drop, act_layer, norm_layer, residual) for i in range(depth)])
         self.norm = norm_layer(hidden_features)
         self.embed_dim = hidden_features
 
     def forward(self, x):
-        x = self.act_layer(self.norm_layer(self.first_layer(self.flatten(x))))
+        x = self.act_layer(self.first_layer(self.flatten(x)))
         for blk in self.blocks:
             x = blk(x)
         x = self.norm(x)